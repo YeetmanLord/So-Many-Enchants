e23b888a1e7d489ea33bbbd1b75b9e86b0da63d0
YeetmanLord <60905636+YeetmanLord@users.noreply.github.com>
2022-06-04 20:23:47 -0600

    stuff

--- src/main/java/com/github/yeetmanlord/somanyenchants/mixins/MixinAnvilMenu.java
+++ src/main/java/com/github/yeetmanlord/somanyenchants/mixins/MixinAnvilMenu.java
@@ -2,9 +2,10 @@ package com.github.yeetmanlord.somanyenchants.mixins;
 
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -14,6 +15,20 @@ import net.minecraft.world.inventory.AnvilMenu;
 
 @Mixin(AnvilMenu.class)
 public class MixinAnvilMenu {
+
+	@Shadow
+	private void setMaximumCost(int value) {
+
+		throw new IllegalStateException("Mixin MixinAnvilMenu could not shadow setMaximumCost!");
+
+	}
+
+	@Shadow
+	private int getCost() {
+
+		throw new IllegalStateException("Mixin MixinAnvilMenu could not shadow setMaximumCost!");
+
+	}
 
 	@Inject(at = @At("HEAD"), cancellable = true, method = "calculateIncreasedRepairCost(I)I")
 	private static void calculateIncreasedRepairCost(int p_39026_, CallbackInfoReturnable<Integer> callback) {
@@ -31,7 +46,7 @@ public class MixinAnvilMenu {
 	private void createResult(CallbackInfo callback) {
 
 		if (SoManyEnchants.instance != null) {
-			(((AnvilMenu) (Object) this)).setMaximumCost((int) Math.floor((((((AnvilMenu) (Object) this)).getCost() / 2) - 1) * 0.75));
+			this.setMaximumCost(Math.max((int) Math.floor(((this.getCost() / 2) - 1) * 0.75 + 1), 1));
 		}
 
 	}
